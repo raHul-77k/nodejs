2127721100f7fea1d0e4bf9b1ea5650932a6e28a
Rahul Kumar <85149417+raHul-77k@users.noreply.github.com>
2024-03-25 18:58:54 +0530

    Add files via upload
    using this with express js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const PORT = 4000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('My name is Rahul Kumar\n');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) =>{
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form> ');
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.post('/product', (req, res, next) =>{
+  console.log(req.body);
+  res.redirect('/');
 });
+
+app.use('/', (req, res, next) =>{
+  res.send('<h>Hello From Express!</h>');
+});
+
+app.listen(3000)
